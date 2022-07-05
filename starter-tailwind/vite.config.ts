2d0c04ebdc6481ccc1958b6d575ab1137653fa53
Luis Martinez <luis@passionatepeople.io>
2022-07-05 10:57:17 +0200

    chore: get rid of tests

--- starter-tailwind/vite.config.ts
+++ starter-tailwind/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const path = require("path");
 // eslint-disable-next-line import/no-unresolved
 import Icons from "unplugin-icons/vite";
 import { alias } from "./alias";
